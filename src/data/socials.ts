9e8a89ced9ce445d76c2cf3ae74b4da9fdcc8c50
Chai <keshab3886@gmail.com>
2025-08-12 21:57:26 +0545

    feat: enhance portfolio with new components and layout improvements
    - Updated .gitignore to include resume files.
    - Modified package.json to add a build script for the resume.
    - Improved the ContactSection component with a new layout and dynamic email link.
    - Enhanced the Footer component with social media links and a back-to-top button.
    - Updated the Hero, AboutMe, Experience, and Projects components for better responsiveness and styling.
    - Added a new font preload link in the Head component for improved performance.
    - Refactored the Navigation component for better accessibility and mobile responsiveness.

---
+++ src/data/socials.ts
@@ -0,0 +1,6 @@
+export const socials = {
+    github: "https://github.com/ChaiKeshab",
+    linkedin: "https://www.linkedin.com/in/keshab-chai/",
+    twitter: "https://x.com/Chai2916",
+    email: "keshab.chai0@gmail.com",
+};
